36180b41063dc0f62a84fef6ef086b1488bb2f29
Stephanie Garcia <sjagarci@fiu.edu>
2026-02-26 01:00:29 -0500

    Completing Data Frame Challenges

--- doc/06_data-frames.R
+++ doc/06_data-frames.R
@@ -18,7 +18,7 @@ cats_2
 # Adding rows (serves as a list)
 ## add a new case to a data frame -- not that the dimensions must be the same 
 age <- c(2, 3)
-cats_2 <- cbind(cats_2, age) #error since dimensions are not the same 
+### cats_2 <- cbind(cats_2, age) #error since dimensions are not the same 
 
 #How do identify what the dimensions are for data frame ? 
 ##nrow(): number of rows
@@ -69,3 +69,61 @@ install.packages("skimr")
 library(skimr)
 skim(gapminder)
 
+# Challenge 1: Create a new data frame directly in R; 
+## use rbind to add entry for someone next to be 
+## use cbind to add a column for the question: "Is it time for a coffee break?"
+df <-  data.frame(id = c("a", "b", "c"),
+                  x = 1:3,
+                  y = c(TRUE, TRUE, FALSE))
+
+me <- data.frame(fname = "Stephanie",
+                 lname = "Garcia",
+                 lucky_number = 8)
+
+next2me <- list("Ludys", "Garcia", 9)
+
+people <- rbind(me, next2me)
+people
+
+coffee_break <- c("No", "Yes")
+info <- cbind(people, coffee_break)
+info
+
+# Solution from notes: 
+df <- data.frame(first = c("Grace"),
+                 last = c("Hopper"),
+                 lucky_number = c(0))
+df <- rbind(df, list("Marie", "Curie", 238) )
+df <- cbind(df, coffeetime = c(TRUE,TRUE))
+df
+
+# Challenge 2: Check last few lines of data; check middle of dataset 
+getwd()
+library(readr)
+gapminder_data <- read.csv("data/gapminder_data.csv")
+
+??head
+head(gapminder_data) # gives first 6 lines of data 
+
+library(dplyr)
+head(gapminder_data, 10) # first 10 rows 
+tail(gapminder_data, 10) # last 10 rows 
+middle_rows <- gapminder_data[847:857,] #extract rows 847-857
+middle_rows
+
+## checking arbitrary rows just in case something is odd 
+gapminder_data[sample(nrow(gapminder_data), 5), ] #sample 5 random rows 
+
+# Challenge 3: Create new R script to load gapminder dataset
+## Place in script folder 
+## Run the script using the source function
+source(file = "scripts/load_gapminder.R")
+
+# Challenge 4: Read output of gapminder_data using str(); explain all parts
+str(gapminder_data)
+## There are 1704 observations and 6 variables: 
+###  character strings: country, continent, life expectancy, GPD per capital
+###  integer vector: year 
+###  numeric vectors: population, life expectancy, GPD per capital
+colnames(gapminder_data) # lists the variables in gapminder dataset
+dim(gapminder_data) #1704 rows, 6 columns 
---
+++ scripts/load_gapminder.R
@@ -0,0 +1,6 @@
+# Referencing 06_data-frames.R 
+# Challenge 3: Create new R script to load gapminder dataset
+## Place in script folder 
+## Run the script using the source function
+library(readr)
+gapminder_data <- read.csv("data/gapminder_data.csv")
